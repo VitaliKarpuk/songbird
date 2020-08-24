2bd0fc1d6c1edff6aae05e21f65382ddf638d663
Vitali Karpuk <karpuk-job@yandex.ru>
2020-08-24 20:58:02 +0300

    feat: block options

--- src/components/BlockAnswerOptions/BlockAnswerOptions.tsx
+++ src/components/BlockAnswerOptions/BlockAnswerOptions.tsx
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import BlockOptions from "./BlockOptions/BlockOptions";
 import BlockDescriptions from "./BlockDescription/BlockDescriptions";
-import { Item } from '../../reducers/reducers';
-import './style.scss'
+import { Item } from "../../reducers/reducers";
+import "./style.scss";
 
 type Props = {
-  listOptions: Array<Item>,
-  handelOption: (id : number) => void,
-  numberOption: number 
-}
-const BlockAnswerOptions: React.FC<Props> = (props : Props) : JSX.Element => {
-  const { listOptions, handelOption, numberOption } = props;
+  listOptions: Array<Item>;
+  handelOption: (id: number) => void;
+  numberOption: number;
+  question: any;
+  handleNextQuestion: () => void;
+  numberQuestion: number;
+  activeBtn: boolean;
+  activeNextBtn: () => void;
+  handleCorrectAnswer: () => void;
+  showScore: () => void;
+};
+const BlockAnswerOptions: React.FC<Props> = (props: Props): JSX.Element => {
+  const {
+    listOptions,
+    handelOption,
+    numberOption,
+    question,
+    handleNextQuestion,
+    numberQuestion,
+    activeBtn,
+    activeNextBtn,
+    handleCorrectAnswer,
+    showScore
+  } = props;
+  
   return (
-    <div className='wrapper__option-description'>
-      <BlockOptions listOptions={listOptions} handelOption={handelOption}/>
-      <BlockDescriptions numberOption={numberOption} listOptions={listOptions}/>
+    <div className="wrapper__option-description">
+      {question && (
+        <BlockOptions
+          question={question}
+          listOptions={listOptions}
+          handelOption={handelOption}
+          activeNextBtn={activeNextBtn}
+          activeBtn={activeBtn}
+          handleCorrectAnswer={handleCorrectAnswer}
+          showScore={showScore}
+        />
+      )}
+      <BlockDescriptions
+        numberOption={numberOption}
+        listOptions={listOptions}
+        numberQuestion={numberQuestion}
+      />
+      {!activeBtn ? (
+        <button className="next-question">Next</button>
+      ) : (
+        <button
+          className="next-question next-question_active"
+          onClick={handleNextQuestion}
+        >
+          Next Level
+        </button>
+      )}
     </div>
   );
 };
--- src/components/BlockAnswerOptions/BlockOptions/BlockOptions.tsx
+++ src/components/BlockAnswerOptions/BlockOptions/BlockOptions.tsx
@@ -1,30 +1,78 @@
-import React from "react";
-import { Item } from '../../../reducers/reducers';
+import React, { useState, useEffect, useRef } from "react";
+import { Item } from "../../../reducers/reducers";
 import "./style.scss";
-
 
 type Props = {
   listOptions: Array<Item>;
-  handelOption: (id: number) => void
+  handelOption: (id: number) => void;
+  question: {};
+  activeNextBtn: () => void;
+  activeBtn: boolean;
+  handleCorrectAnswer: () => void;
+  showScore: (number: number) => void;
 };
 
-
+const urlError =
+  "https://zvukipro.com/uploads/files/2019-06/1561356080_b5f0886abfa6621.mp3";
+const urlWin =
+  "https://zvukipro.com/uploads/files/2019-10/1571643581_win31.mp3";
 
 const BlockOptions: React.FC<Props> = (props: Props) => {
-  const { listOptions, handelOption } = props;
+  const {
+    listOptions,
+    handelOption,
+    question,
+    activeNextBtn,
+    activeBtn,
+    handleCorrectAnswer,
+    showScore,
+  } = props;
+  const [list, setList] = useState(listOptions);
+  const [scoreQuestion, setScoreQuestion] = useState(5);
+  const audio = useRef("audio_tag");
+
+  const handleAnswer = (id: number) => {
+    handelOption(id);
+    if (!activeBtn) {
+      const listAnswers = list.map((item) => {
+        if (item.id === id && question.id !== id) {
+          setScoreQuestion((prev) => prev - 1);
+          audio.current.src = urlError;
+          audio.current.play();
+          return { ...item, classSpan: "incorrect_answer" };
+        } else if (item.id === id && question.id === id) {
+          audio.current.src = urlWin;
+          audio.current.play();
+          activeNextBtn();
+          handleCorrectAnswer();
+          showScore(scoreQuestion);
+          setScoreQuestion(5);
+          return { ...item, classSpan: "correct_answer" };
+        } else {
+          return item;
+        }
+      });
+      setList(listAnswers);
+    }
+  };
+
+  useEffect(() => {
+    setList(listOptions);
+  }, [listOptions]);
 
   return (
     <div className="wrapper__option">
       <ul>
-        {listOptions.map((item) => {
+        {list.map((item) => {
           return (
-            <li key={item.id} onClick={() => handelOption(item.id)}>
-              <span></span>
+            <li key={item.id} onClick={() => handleAnswer(item.id)}>
+              <span className={item.classSpan}></span>
               {item.name}
             </li>
           );
         })}
       </ul>
+      <audio ref={audio} />
     </div>
   );
 };
