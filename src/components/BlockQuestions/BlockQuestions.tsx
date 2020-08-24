f8cd61faf066ec201c9742f5733ec60f82932265
Vitali Karpuk <karpuk-job@yandex.ru>
2020-08-24 20:58:36 +0300

    feat: block question

--- src/components/BlockQuestions/BlockQuestions.tsx
+++ src/components/BlockQuestions/BlockQuestions.tsx
@@ -1,21 +1,35 @@
-import React from "react";
-import Player from '../Player/Player';
+import React, { useEffect, useState } from "react";
+import Player from "../Player/Player";
+import { Item } from "../../reducers/reducers";
+import img from "../../images/hidden-bird.jpg";
 
 import "./style.scss";
 
-const BlockQuestions2: React.FC = () => {
+type Props = {
+  question: any;
+  showCorrectAnswer: boolean;
+};
 
+const BlockQuestions2: React.FC<Props> = (props: Props) => {
+  const { question, showCorrectAnswer } = props;
 
   return (
     <div className="block-questions">
       <div className="image-bird">
-        <span className="hidden-bird"></span>
+        <span
+          className="hidden-bird"
+          style={{
+            backgroundImage: `url('${
+              !showCorrectAnswer ? img : question.image
+            }')`,
+          }}
+        ></span>
       </div>
       <div className="song-bird">
         <div className="title__bird">
-          <h3>*************</h3>
+          <h2>{!showCorrectAnswer ? "***" : question.name}</h2>
         </div>
-        <Player  />
+        {question && <Player audio={question.audio} autoPlay={false} />}
       </div>
     </div>
   );
